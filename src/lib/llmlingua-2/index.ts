aefed33de2d4193aa9999a34ee110279ce74ae1e
its-iris <244833241+its-iris@users.noreply.github.com>
2026-04-02 10:59:33 +0200

    fix: fixed type exports

--- src/lib/llmlingua-2/index.ts
+++ src/lib/llmlingua-2/index.ts
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: MIT
 
-export {
-  PromptCompressorLLMLingua2 as PromptCompressor,
+export { PromptCompressorLLMLingua2 as PromptCompressor } from "./prompt-compressor.js";
+export type {
   CompressPromptOptions,
   CompressPromptOptionsSnakeCase,
 } from "./prompt-compressor.js";
 export {
   get_pure_tokens_bert_base_multilingual_cased,
   get_pure_tokens_xlm_roberta_large,
-  GetPureTokenFunction,
   is_begin_of_new_word_bert_base_multilingual_cased,
   is_begin_of_new_word_xlm_roberta_large,
+} from "./utils.js";
+export type {
+  GetPureTokenFunction,
   IsBeginOfNewWordFunction,
 } from "./utils.js";
-export {
-  WithXLMRoBERTa,
-  WithBERTMultilingual,
+export { WithXLMRoBERTa, WithBERTMultilingual } from "./factory.js";
+export type {
   LLMLingua2FactoryOptions as FactoryOptions,
   LLMLingua2FactoryReturn as FactoryReturn,
 } from "./factory.js";
